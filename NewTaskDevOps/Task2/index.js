d1ddfbf010ec5c1d7e491f5efffca3db3c265f1e
Morgunov Mikhail <mikhail.morgunov@smiledirectclub.com>
2024-01-15 11:28:44 +0300

    fix task2

--- NewTaskDevOps/Task2/index.js
+++ NewTaskDevOps/Task2/index.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Test task completed successfully!\n');
+res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8'});
+res.end('Тестовое задание выполнено успешно!\n');
 });
 
 const port = 3000;
 
 server.listen(port, () => {
-    console.log(`Server listens on port ${port}`);
+console.log(`Сервер слушает на порту ${port}`);
 });
